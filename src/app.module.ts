f824b5d65a2dbfc2a6edf3a1b5624823cfae4591
HeymerDev <heymermeza11@gmail.com>
2026-09-16 22:24:42 -0500

    feat: active validation pipes and instal class-validator & import catgories module o app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { CategoriesModule } from './categories/categories.module.js';
 
 @Module({
-  imports: [],
+  imports: [CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
