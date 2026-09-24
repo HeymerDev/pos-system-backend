13c6c50cae57cb578f95a56f103f0246d202bafb
HeymerDev <heymermeza11@gmail.com>
2026-09-23 23:08:07 -0500

    feat: add categories service

---
+++ src/categories/categories.service.ts
@@ -0,0 +1,45 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateCategoryDto } from './dto/create-category.dto.js';
+import { UpdateCategoryDto } from './dto/update-category.dto.js';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from './entities/category.entity.js';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class CategoriesService {
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>,
+  ) {}
+
+  create(createCategoryDto: CreateCategoryDto) {
+    return this.categoryRepository.save(createCategoryDto);
+  }
+
+  findAll() {
+    return this.categoryRepository.find();
+  }
+
+  async findOne(id: number) {
+    const category = await this.categoryRepository.findOneBy({ id });
+    if (!category) {
+      throw new NotFoundException(`Category with ID ${id} not found`);
+    }
+
+    return category;
+  }
+
+  async update(id: number, updateCategoryDto: UpdateCategoryDto) {
+    const category = await this.findOne(id);
+    category.name = updateCategoryDto.name;
+    await this.categoryRepository.save(category);
+
+    return category;
+  }
+
+  async remove(id: number) {
+    const category = await this.findOne(id);
+    await this.categoryRepository.delete(category);
+    return 'Category deleted successfully';
+  }
+}
