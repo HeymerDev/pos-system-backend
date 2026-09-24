f04f7981dbdf5b48accb3746bc66e0ec3cdcc6e7
HeymerDev <heymermeza11@gmail.com>
2026-09-23 23:02:54 -0500

    feat: id pipe validation that id is number not word

---
+++ src/common/pipes/id-validation-pipe/id-validation-pipe.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { IdValidationPipePipe } from './id-validation-pipe.pipe.js';
+
+describe('IdValidationPipePipe', () => {
+  it('should be defined', () => {
+    expect(new IdValidationPipePipe()).toBeDefined();
+  });
+});
