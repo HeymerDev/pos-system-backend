c10b64d4e456635f4c9abb69421d7e147ec071ae
HeymerDev <heymermeza11@gmail.com>
2026-09-25 22:03:51 -0500

    feat: add create product logic service

---
+++ src/products/products.service.ts
@@ -0,0 +1,48 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto.js';
+import { UpdateProductDto } from './dto/update-product.dto.js';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity.js';
+import { Repository } from 'typeorm';
+import { Category } from '../categories/entities/category.entity.js';
+
+@Injectable()
+export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    const category = await this.categoryRepository.findOne({
+      where: { id: createProductDto.categoryId },
+    });
+
+    if (!category) {
+      let errors: string[] = [];
+      errors.push('Category not found');
+      throw new NotFoundException(errors);
+    }
+
+    return this.productRepository.save({ ...createProductDto, category });
+  }
+
+  findAll() {
+    return `This action returns all products`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} product`;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+}
